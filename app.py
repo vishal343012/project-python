157fcb9c1615831f77bdc74c18668dd3fed7f1c1
vishal343012 <119778329+vishal343012@users.noreply.github.com>
2023-12-28 14:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def hello():
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
     html = "<h3> "HAPPY NEW YEAR" 
-            
-           "<b>Visits:</b> {visits}"
+            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
