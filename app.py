4169a48576ae4d2bd968d15a9deb59b8626aa31a
vishal343012 <119778329+vishal343012@users.noreply.github.com>
2024-03-09 12:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3> "HAPPY NEW YAR" 
+    html = "<h3> "HAPPY NEW YEAR" 
             "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
